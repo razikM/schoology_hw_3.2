a0f783ff73b49aedb7ce1db11c3856f6ce730063
Misha Razinkin <razinkin.m.v@gmail.com>
2021-10-22 14:27:58 +0200

    adding data to created columns in 1 and 4.sql

--- 1.sql
+++ 1.sql
@@ -1,2 +1,14 @@
 alter table developers add column salary int;
 
+update developers
+set salary = 1000
+where id = 1;
+
+update developers
+set salary = 2000
+where id = 2;
+
+update developers
+set salary = 3000
+where id = 3;
+
--- 4.sql
+++ 4.sql
@@ -1,1 +1,13 @@
 alter table projects add column cost int;
+
+update projects
+set cost = 10000
+where id = 1;
+
+update projects
+set cost = 20000
+where id = 2;
+
+update projects
+set cost = 30000
+where id = 3;
